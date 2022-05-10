3b2955d582f409b56576fb0a3bfa45bd320d4bd7
T.Bobur <b.turabov@imkon.uz>
2022-05-10 13:29:16 +0500

    nothing

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // base: '/some/'
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/production-sub-path/'
-  : '/'
+  publicPath: '/my-first-project/'
 })
