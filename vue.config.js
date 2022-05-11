976a5c1756598c60a31c6317bc907509715d10e5
T.Bobur <b.turabov@imkon.uz>
2022-05-11 15:42:27 +0500

    dist chang

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 })
 
-module.exports = { 
-    publicPath: 'auto',
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "matrix-mathrix" : "/",
 };
